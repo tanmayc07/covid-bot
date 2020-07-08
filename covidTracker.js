5fbe132d77c435d7050dcf9a4a2e1d272916610c
tanmayc07 <tanmayc32@gmail.com>
2020-07-08 20:26:00 +0530

    Added .gitignore

--- covidTracker.js
+++ covidTracker.js
@@ -12,6 +12,7 @@ client.on("ready", () => {
 
 client.on("message", async (msg) => {
   const content = msg.content.split(/[ ,]+/);
+
   if (content[0] === WAKE_COMMAND) {
     if (content.length > 2) {
       msg.reply("Too many arguments...");
@@ -23,6 +24,7 @@ client.on("message", async (msg) => {
       const slug = content[1];
       const payload = await axios.get(`${url}${slug}`);
       const covidData = payload.data.pop();
+
       msg.reply(
         `Confirmed: ${covidData.Confirmed}, Deaths: ${covidData.Deaths}, Recovered: ${covidData.Recovered}, Active: ${covidData.Active} `
       );
